386e4e0f871d340643f71772ba0fcd2821154084
Ahtisham <ahtisham@gosaas.io>
2020-11-12 11:46:06 +0500

    async await syntax used to handle asynchronous code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 console.log('Before');
+// get_user(1)
+//   .then((user) => get_repos(user.name))
+//   .then((repos) => console.log(repos))
+//   .catch((err) => console.log('Error: ', err.message));
 
-get_user(1)
-  .then((user) => get_repos(user.name))
-  .then((repos) => console.log(repos))
-  .catch((err) => console.log('Error: ', err.message));
-
-  console.log('After');
+display_repos();
+console.log('After');
 
 function get_user(id){
   return new Promise((resolve, reject) => {
@@ -25,4 +25,10 @@ function get_repos(username){
       // reject('some error message');
     }, 2000);
   });
+}
+
+async function display_repos(){
+  const user = await get_user(1);
+  const repos = await get_repos(user.name);
+  console.log('Repos', repos);
 }
